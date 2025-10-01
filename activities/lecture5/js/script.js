bbf885e227c121a50d90f3aefefd0efcab536d88
Shruti <sh711918@dal.ca>
2025-10-01 09:28:09 -0300

    File added

---
+++ activities/lecture5/js/script.js
@@ -0,0 +1,13 @@
+// Create an array of possible answers
+
+  
+// Create a function to fetch the question the user has asked 	
+// Our function should also check from an empty value
+
+  
+// Select a random answer from your array 
+
+
+	
+// Display the question and answer back to the user
+// And, log the question and answer to the console
