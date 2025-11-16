85c24b6cbdb2f052bd3091678ff01521cb5897de
Shruti <sh711918@dal.ca>
2025-11-16 17:39:25 -0400

    lab5

--- activities/lecture9/js/script.js
+++ activities/lecture9/js/script.js
@@ -35,3 +35,10 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+
+
+
+
+
--- labs/lab5/my-portfolio/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer-modern">
-      © {new Date().getFullYear()} Shruti Jakhar — Portfolio
-    </footer>
-  );
-}
